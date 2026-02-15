f57af27d16a464cf175f9ac64f0e689d7af15fc8
Kiss Róbert <ert78gb@gmail.com>
2026-02-15 15:21:41 +0100

    await setTimeout(5000);

--- e2e/app-start.spec.js
+++ e2e/app-start.spec.js
@@ -1,3 +1,4 @@
+import { setTimeout } from "node:timers/promises";
 import { expect, test } from "@playwright/test";
 import { _electron } from "playwright";
 
@@ -5,6 +6,7 @@ test("should app start", async () => {
   const electronApp = await _electron.launch({ args: ["./src/main/index.js"] });
 
   const window = await electronApp.firstWindow();
+  await setTimeout(5000);
   const screenshot = await window.screenshot();
   await expect(screenshot).toMatchSnapshot("app-started.png");
   await electronApp.close();
